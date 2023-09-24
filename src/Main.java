00aa0d491f6e6349705df5c06fdefd77e68fb37d
Thoorst <cywinskimateusz@gmail.com>
2023-09-24 20:00:29 +0200

    Add ternary operator approach

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,12 @@
+import java.io.FilterOutputStream;
+
 public class Main {
     public static void main(String[] args) {
+        //Two approaches presented, both are good but the first one is more readable in that case for me
         printDivisibility();
+        //printDivisibilityTerneryOperator();
     }
+    
     static void printDivisibility(){
         for(int i = 1; i <= 100; i++){
             if(i%3==0 && i%5==0) {
@@ -13,6 +18,13 @@ public class Main {
             }else{
                 System.out.println(i);
             }
+        }
+    }
+
+    static void printDivisibilityTerneryOperator(){
+        for(int i =1; i <=100; i++){
+            String output = (i%3==0 && i%5==0 ? "gamma" : (i%3==0 ? "alfa" : (i%5==0 ? "beta" : String.valueOf(i)))) + "\n";
+            System.out.print(output);
         }
     }
 }
